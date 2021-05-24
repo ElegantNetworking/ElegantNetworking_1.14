f52bf5d983ca46d324e20aee7721f0ff3d0c53e0
hohserg <hohserg1@yandex.ru>
2021-05-24 17:13:44 +0300

    Added support for BlockPos. Reorganize methods. Up version 3.4

--- src/main/java/hohserg/elegant/networking/impl/ISerializer.java
+++ src/main/java/hohserg/elegant/networking/impl/ISerializer.java
@@ -13,6 +13,7 @@ import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTSizeTracker;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.ForgeRegistry;
@@ -20,9 +21,16 @@ import net.minecraftforge.registries.ForgeRegistry;
 import java.io.IOException;
 
 public interface ISerializer<Packet> extends ISerializerBase<Packet> {
-    void serialize(Packet value, ByteBuf acc);
 
-    Packet unserialize(ByteBuf buf);
+    default void serialize_BlockPos_Generic(BlockPos value, ByteBuf acc) {
+        acc.writeInt(value.getX());
+        acc.writeInt(value.getY());
+        acc.writeInt(value.getZ());
+    }
+
+    default BlockPos unserialize_BlockPos_Generic(ByteBuf buf) {
+        return new BlockPos(buf.readInt(), buf.readInt(), buf.readInt());
+    }
 
     default void serialize_CompoundNBT_Generic(CompoundNBT value, ByteBuf acc) {
         if (value != null)
@@ -36,31 +44,7 @@ public interface ISerializer<Packet> extends ISerializerBase<Packet> {
             acc.writeBoolean(false);
     }
 
-    default void serialize_ItemStack_Generic(ItemStack value, ByteBuf acc) {
-        new PacketBuffer(acc).writeItemStack(value);
-    }
-
-    default void serialize_FluidStack_Generic(FluidStack value, ByteBuf acc) {
-        new PacketBuffer(acc).writeFluidStack(value);
-    }
-
-    default void serialize_Item_Generic(Item value, ByteBuf acc) {
-        acc.writeInt(((ForgeRegistry<Item>) ForgeRegistries.ITEMS).getID(value));
-    }
-
-    default void serialize_Block_Generic(Block value, ByteBuf acc) {
-        acc.writeInt(((ForgeRegistry<Block>) ForgeRegistries.BLOCKS).getID(value));
-    }
-
-    default void serialize_Fluid_Generic(Fluid value, ByteBuf acc) {
-        acc.writeInt(((ForgeRegistry<Fluid>) ForgeRegistries.FLUIDS).getID(value));
-    }
-
-    default void serialize_ResourceLocation_Generic(ResourceLocation value, ByteBuf acc) {
-        serialize_String_Generic(value.toString(), acc);
-    }
-
-    default CompoundNBT unserialize_NBTTagCompound_Generic(ByteBuf buf) {
+    default CompoundNBT unserialize_CompoundNBT_Generic(ByteBuf buf) {
         if (buf.readBoolean())
             try {
                 return CompressedStreamTools.read(new ByteBufInputStream(buf), new NBTSizeTracker(2097152L));
@@ -71,24 +55,48 @@ public interface ISerializer<Packet> extends ISerializerBase<Packet> {
             return null;
     }
 
+    default void serialize_ItemStack_Generic(ItemStack value, ByteBuf acc) {
+        new PacketBuffer(acc).writeItemStack(value);
+    }
+
     default ItemStack unserialize_ItemStack_Generic(ByteBuf buf) {
         return new PacketBuffer(buf).readItemStack();
+    }
+
+    default void serialize_FluidStack_Generic(FluidStack value, ByteBuf acc) {
+        new PacketBuffer(acc).writeFluidStack(value);
     }
 
     default FluidStack unserialize_FluidStack_Generic(ByteBuf buf) {
         return new PacketBuffer(buf).readFluidStack();
     }
 
+    default void serialize_Item_Generic(Item value, ByteBuf acc) {
+        acc.writeInt(((ForgeRegistry<Item>) ForgeRegistries.ITEMS).getID(value));
+    }
+
     default Item unserialize_Item_Generic(ByteBuf buf) {
         return ((ForgeRegistry<Item>) ForgeRegistries.ITEMS).getValue(buf.readInt());
+    }
+
+    default void serialize_Block_Generic(Block value, ByteBuf acc) {
+        acc.writeInt(((ForgeRegistry<Block>) ForgeRegistries.BLOCKS).getID(value));
     }
 
     default Block unserialize_Block_Generic(ByteBuf buf) {
         return ((ForgeRegistry<Block>) ForgeRegistries.BLOCKS).getValue(buf.readInt());
     }
 
+    default void serialize_Fluid_Generic(Fluid value, ByteBuf acc) {
+        acc.writeInt(((ForgeRegistry<Fluid>) ForgeRegistries.FLUIDS).getID(value));
+    }
+
     default Fluid unserialize_Fluid_Generic(ByteBuf buf) {
         return ((ForgeRegistry<Fluid>) ForgeRegistries.FLUIDS).getValue(buf.readInt());
+    }
+
+    default void serialize_ResourceLocation_Generic(ResourceLocation value, ByteBuf acc) {
+        serialize_String_Generic(value.toString(), acc);
     }
 
     default ResourceLocation unserialize_ResourceLocation_Generic(ByteBuf buf) {
