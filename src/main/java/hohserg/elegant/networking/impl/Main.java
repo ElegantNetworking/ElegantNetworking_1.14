5f1fca4558332dc40ae4ef0afdac2584bb1c5b64
hohserg <hohserg1@yandex.ru>
2021-07-07 15:57:10 +0300

    Use ServiceLoader

--- src/main/java/hohserg/elegant/networking/impl/Main.java
+++ src/main/java/hohserg/elegant/networking/impl/Main.java
@@ -1,27 +1,22 @@
 package hohserg.elegant.networking.impl;
 
-import net.minecraftforge.fml.ModList;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
 
 @Mod("elegant_networking")
+@Mod.EventBusSubscriber(bus = MOD)
 public class Main {
     public static Logger log = LogManager.getLogger("elegant_networking");
     public static Config config = Init.initConfig(FMLPaths.CONFIGDIR.get().toFile());
 
-    public Main() {
-        Init.registerAllPackets(
-                ModList.get().getMods()
-                        .stream()
-                        .map(mod -> new Init.ModInfo(mod.getModId(), mod.getOwningFile().getFile().getFilePath().toFile()))
-                        .collect(Collectors.toList()),
-                log::info,
-                log::error,
-                Network.getNetwork()::registerChannel
-        );
+    @SubscribeEvent
+    public static void onCommonSetup(FMLCommonSetupEvent event) {
+        Init.initPackets(log::info, log::error, Network.getNetwork()::registerChannel);
     }
 }
